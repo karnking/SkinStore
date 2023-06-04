55fa375959cc765372b5b59122d464647d48f4a6
karnking <karankotai9183@gmail.com>
2023-06-04 14:15:08 +0530

    made basic of cart page

--- scripts/productGrid.js
+++ scripts/productGrid.js
@@ -20,12 +20,19 @@ for(x in featured){
 }
 //product grid part
 var tab = localStorage.getItem("tab");
-
+var tempArr = productsArray[tab].arr.slice();
 function showGrid(elem,index){
     var boxDiv = document.createElement("div")
     var heartIcon = document.createElement("i");
     heartIcon.setAttribute("class","fa-regular fa-heart")
-    heartIcon.addEventListener("click",wishList)
+    heartIcon.addEventListener("click",function(){
+        wishList(elem);
+        if(heartIcon.style.fontWeight != "900"){
+            heartIcon.style.fontWeight = "900"
+        }else{
+            heartIcon.style.fontWeight = "100"
+        }
+    })
     var boxImage = document.createElement("img")
     boxImage.src = elem.main_img;
     boxImage.addEventListener("mouseout",function(){
@@ -34,9 +41,17 @@ function showGrid(elem,index){
     boxImage.addEventListener("mouseover",function(){
         boxImage.src = elem.hover_img;
     })
+    boxImage.addEventListener("click", function (){
+        localStorage.setItem("showProduct",index)
+        window.location.href = "../productDetail.html"
+    });
     var subBox = document.createElement("div")
     var boxh3 = document.createElement("h3")
     boxh3.textContent = elem.title
+    boxh3.addEventListener("click", function (){
+        localStorage.setItem("showProduct",index)
+        window.location.href = "../productDetail.html"
+    });
     var giftInp = document.createElement("input");
     giftInp.setAttribute("type","button")
     giftInp.setAttribute("value",giftsArray[0].sec_title)
@@ -60,20 +75,20 @@ function showGrid(elem,index){
     var boxh5 = document.createElement("h5")
     boxh5.innerHTML = `$${elem.aPrice}`
     var boxbtn = document.createElement("button")
-    boxbtn.textContent = "SHOP NOW"
+    boxbtn.textContent = "QUICK BUY"
     boxbtn.addEventListener("click", function (){
-        localStorage.setItem("showProduct",index)
-        window.location.href = "../productDetail.html"
+        addToCart(elem);
     });
     subBox.append(boxh3, giftInp, starDiv, boxp, boxh5)
     boxDiv.append(heartIcon,boxImage, subBox, boxbtn)
     document.querySelector("#product-grid").append(boxDiv)
 };
 function initialShow(){
+    document.querySelector("#product-grid").innerHTML = ""
     document.querySelector("#productGrid #page>h1").textContent = productsArray[tab].detail.heading;
     document.querySelector("#productGrid #page>p").textContent = productsArray[tab].detail.qt;
     document.querySelector("#productGrid #page>.page-desc").textContent = productsArray[tab].detail.page_desc;
-    productsArray[tab].arr.map(function (elem,index) {
+    tempArr.map(function (elem,index) {
         showGrid(elem,index);
     })
 }
@@ -89,6 +104,7 @@ document.getElementById("read-more").addEventListener("click",function(){
         p.style.maxHeight = "60px"; 
     }
 })
+
 document.querySelectorAll("#productGrid>div:last-child>div:first-child>h3").forEach(function(ele){
     ele.addEventListener("click",function(){
         ele.classList.toggle("h3-active")
@@ -97,29 +113,89 @@ document.querySelectorAll("#productGrid>div:last-child>div:first-child>h3").forE
         if(d.classList.contains("check-options-active")){
             d.style.maxHeight = "0";
         }else{
-            d.style.maxHeight = "6em";
+            d.style.maxHeight = "6.1em";
         }
     })
 })
-document.querySelectorAll("#productGrid>div:last-child>div:first-child>div").forEach(function(elem){
-    
+
+document.querySelectorAll("#productGrid .check-options input").forEach(function(ele){
+    ele.addEventListener("click",function(){
+        tempArr = productsArray[tab].arr;
+        var name = ele.name;
+        var value = ele.value;
+        if(name==="gender"){
+            document.querySelectorAll("#productGrid .check-options input[name='price'],#productGrid .check-options input[name='type'],#productGrid .check-options input[name='review']").forEach(function(elem){
+                elem.checked = false;
+            })
+            if(value!='all'){
+                tempArr = tempArr.filter(function(elem){
+                    return elem[name]==value;
+                })
+            }
+        }else if(name==="price"){
+            document.querySelectorAll("#productGrid .check-options input[name='gender'],#productGrid .check-options input[name='type'],#productGrid .check-options input[name='review']").forEach(function(elem){
+                elem.checked = false;
+            })
+            if(value!='all'){
+                tempArr = tempArr.filter(function(elem){
+                    if(value==="ten"){
+                        return elem.aPrice<10;
+                    }else if(value==="tf"){
+                        return elem.aPrice <= 25 && elem.aPrice >= 10;
+                    }else if(value==="fifty"){
+                        return elem.aPrice <= 50 && elem.aPrice >= 25;
+                    }else if(value==="hund"){
+                        return elem.aPrice <= 100 && elem.aPrice >= 50;
+                    }else if(value==="mth"){
+                        return elem.aPrice > 100;
+                    }
+                })
+            }
+        }else if(name==="review"){
+            document.querySelectorAll("#productGrid .check-options input[name='price'],#productGrid .check-options input[name='type'],#productGrid .check-options input[name='gender']").forEach(function(elem){
+                elem.checked = false;
+            })
+            if(value!='all'){
+                tempArr = tempArr.filter(function(elem){
+                    if(value==="review_2"){
+                        return elem.reviews>=2 && elem.reviews<=3;
+                    }else if(value==="review_3"){
+                        return elem.reviews>=3 && elem.reviews<=4;
+                    }else if(value==="review_4"){
+                        return elem.reviews>4;
+                    }
+                })
+            }
+        }else if(name==="type"){
+            document.querySelectorAll("#productGrid .check-options input[name='price'],#productGrid .check-options input[name='gender'],#productGrid .check-options input[name='review']").forEach(function(elem){
+                elem.checked = false;
+            })
+            if(value!='all'){
+                tempArr = tempArr.filter(function(elem){
+                    return elem[name]==value;
+                })
+            }
+        }
+        initialShow()
+    })
 })
 document.querySelector("#productGrid #page>div:nth-child(5)>select").addEventListener("change",function(){
+    tempArr = productsArray[tab].arr;
     var sorter = document.querySelector("#productGrid #page>div:nth-child(5)>select").value;
     if(sorter==="pop"){
-        var newArr = productsArray[tab].arr.sort(function(a,b){
+        tempArr.sort(function(a,b){
             return Number(b.reviews)-Number(a.reviews);
         })  
     }else if(sorter==="lth"){
-        var newArr = productsArray[tab].arr.slice().sort(function(a,b){
+        tempArr.sort(function(a,b){
             return Number(a.aPrice)-Number(b.aPrice);
         })
     }else if(sorter==="htl"){
-        var newArr = productsArray[tab].arr.slice().sort(function(a,b){
+        tempArr.sort(function(a,b){
             return Number(b.aPrice)-Number(a.aPrice);
         })
     }else if(sorter==="az"){
-        var newArr = productsArray[tab].arr.slice().sort(function(a,b){
+        tempArr.sort(function(a,b){
             let x = a.title.toLowerCase();
             let y = b.title.toLowerCase();
             if (x < y) {return -1;}
@@ -127,21 +203,30 @@ document.querySelector("#productGrid #page>div:nth-child(5)>select").addEventLis
             return 0;
         })
     }else if(sorter==="random"){
-        var newArr = productsArray[tab].arr.slice().sort(function(a,b){
+        tempArr.sort(function(a,b){
             return Math.floor(Math.random() * (3) ) - 1;
         })
     }else if(sorter===""){
-        var newArr = productsArray[tab].arr.slice();
+        tempArr = productsArray[tab].arr.slice();
     }
-    document.querySelector("#product-grid").innerHTML = ""
-    newArr.map(function (elem,index) {
-        showGrid(elem,index);
-    })
+    initialShow()
 })
 
 function popUp(){
 
 }
-function wishList(){
-
+var wishListArr = JSON.parse(localStorage.getItem("wishList")) || []
+function wishList(elem){
+    var idx = wishListArr.indexOf(elem);
+    if(idx!=-1){
+        wishListArr.splice(idx,1);
+    }else{
+        wishListArr.push(elem);
+    }
+    localStorage.setItem("wishlist",JSON.stringify(wishListArr));
+}
+var cart = JSON.parse(localStorage.getItem("cart")) || []
+function addToCart(elem){
+    cart.push(elem);
+    localStorage.setItem("cart",JSON.stringify(cart));
 }
--- scripts/productsArray.js
+++ scripts/productsArray.js
@@ -7,17 +7,21 @@ var productsArray = {
         },
         arr : [
             {
+                id: "cl1",
                 main_img: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11535230-2295043599450781.jpg",
                 hover_img: "https://static.thcdn.com/images/small/webp//productimg/original/11535230-8544892768833011.jpg",
                 title: "SkinCeuticals Gentle Cleanser 6.8 fl. oz",
-                bPrice: "36.00",
+                bPrice: "38.00",
                 aPrice: "36.00",
                 desc: "Experience a mild yet effective cleanse with SkinCeuticals Gentle Cleanser, a non-foaming face wash that performs a delicate cleanse while conditioning the skin. Suitable for daily use, and for sensitive skin, the gentle, creamy formula has been developed with an emollient-rich surfactant system, and contains a 3% concentration comprising Allantoin, Glycerin and Orange Oil that nourishes and resurfaces skin to leave it feeling soft and supple. Deeply moisturizing, the face wash expertly lifts away dirt and makeup, including long-wearing products, while replenishing and priming skin ready for the next steps in your skincare regimen. After use, skin feels clean, fresh and comfortable with renewed softness. Suitable for dry, normal, sensitive, reactive, or post-procedure skin. Non-comedogenic. Free from parabens, sulfates, soap, synthetic-fragrance, dye and alcohol.",
                 ingredients: "Aqua / Water / Eau, C12-15 Alkyl Benzoate, Glycerin, Propanediol, Cetyl Alcohol, Methyl Gluceth-20, Neopentyl Glycol Dicaprate, Peg-100 Stearate, Glyceryl Stearate, Polyglyceryl-4 Caprate, Phenoxyethanol, Citrus Aurantium Dulcis Oil / Orange Peel Oil, Limonene, Acrylates/C10-30 Alkyl Acrylate Crosspolymer, Capryl/Capramidopropyl Betaine, Caprylyl Glycol, Polysorbate 20, Allantoin, Sodium Hydroxide, Pentaerythrityl Tetra-Di-T-Butyl Hydroxyhydrocinnamate, Disodium Edta, Polyquaternium-39, Sodium Chloride, Peg-30 Dipolyhydroxystearate, Trideceth-6, Glycolic Acid, Sodium Benzoate.",
                 procedure: ["Twice daily, apply a small amount to a wet face and neck with light circular motion.", "Rinse thoroughly with water."],
-                reviews: 5
+                reviews: 5,
+                gender: "men",
+                type: "oily"
             },
             {
+                id: "cl2",
                 main_img: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11370307-1475014852189893.jpg",
                 hover_img: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11370307-3954944342301413.jpg",
                 title: "EltaMD Foaming Facial Cleanser 207ml",
@@ -26,9 +30,12 @@ var productsArray = {
                 desc: "Elta MD Foaming Facial Cleanser is an innovative formula with a bromelain and apple amino acid blend. Its self-foaming action gently yet thoroughly removes makeup, oil and other impurities. Now patients with even the most sensitive skin can experience deep-pore cleansing without drying or irritating their skin. Suited for all skin types. <b>Who is this for?</b><br>Ideal for those who want an oil-free cleanser. Suitable for all skin types.",
                 ingredients: " <b>Key Ingredients:</b><br>Bromelain: Enzymes found in pineapples that work as an anti-inflammatory and reduces inflammation. Adds the foaming feature, leaving skin silky smooth.<br> <b>Full Ingredients:</b><br/>Purified Water, Sodium Cocoyl Glycinate, Acrylates Copolymer, Sodium Cocoyl Apple Amino Acids, Methyl Perfluoroisobutyl Ether, Magnesium Silicate, Methyl Perfluorobutyl Ether, Bromelain, Dimethicone Copolyol, Polyhydroxystearic Acid, Isononyl Isononanoate, Ethylhexyl Isononanoate, Sodium Cocamidopropyl PG-Dimonium Chloride Phosphate, Phenoxyethanol, Sodium Hydroxide, Capryloyl Glycine, Undecylenoyl Glycine, Sodium Bisulfite, Fragrance.",
                 procedure: ["On damp skin, gently massage in circular motion. Allow 30 seconds for cleanser to foam. Rinse thoroughly."],
-                reviews: 3
+                reviews: 3,
+                gender: "women",
+                type: "dry"
             },
             {
+                id: "cl3",
                 main_img: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11165912-1924943596087530.jpg",
                 hover_img: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11165912-2004943596135047.jpg",
                 title: "Sarah Chapman Skinesis Rapid Radiance Cleanse (100ml)",
@@ -37,10 +44,49 @@ var productsArray = {
                 desc: "The Sarah Chapman Skinesis Rapid Radiance Cleanse is an indulgent creamy cleanser, that doubles as a 2-minute detoxifying mask due to its blend of rare clays. The multi-functioning, radiance-boosting treatment is powered by lactic and salicylic acids. The exfoliating formula helps to buff away dead skin cells, while French red clay, PoreAway and Detoxophane complexes seek to decongest, for a clarified, refined texture. Fortified with vitamin C, the cooling cleanser encourages a complexion that appears brighter with enhanced radiance, alongside a cocktail of vitamin oils nourishing the visage and melting away impurities.",
                 ingredients: "Aqua (Water), Glycerin, Prunus Amygdalus Dulcis (Sweet Almond) Oil, Decyl Glucoside, Carthamus Tinctorius (Safflower) Seed Oil, Stearic Acid, Glyceryl Stearate SE, Pentaclethra Macroloba Seed Oil, Butyrospermum Parkii (Shea) Butter, Glycol Stearate, Kaolin, Cocamidopropyl Betaine, Illite, Cetearyl Alcohol, Betaine, Magnesium Aluminium Silicate, Phenoxyethanol, Lepidium Sativum Sprout Extract, Citrus Bergamia Fruit Oil, Salicylic Acid, Limonene, Sodium Ascorbyl Phosphate, Xanthan Gum, Linalool, Lactic Acid, Benzyl Alcohol, Sodium Hydroxide, Citrus Aurantium Amara Leaf/Twig Oil, Alcohol, Cyclodextrin, Calendula Officinalis Flower Extract, Ethylhexylglycerin, Menthol, Lecithin, Disodium EDTA, Sodium Benzoate, Menthyl Lactate, Geraniol, Dehydroacetic Acid, Pistacia Lentiscus (Mastic) Gum, Pelargonium Graveolens Oil, Citral, Potassium Sorbate, Citronellol, Citric Acid, Citrus Aurantium Amara Flower Oil.",
                 procedure: ["<i class='fa-regular fa-sun'></i> AM / <i class='fa-regular fa-moon'></i> PM", "When to Use: ​The first step in your routine.", "Recommended Amount:​ 2-4 pumps.", "Application:​ Massage onto a dry face and neck and rinse. For use as a mask, leave on for 2 minutes and rinse.", "Routine: Follow with serums, moisturiser and SPF."],
-                reviews: 4
+                reviews: 4,
+                gender: "boys",
+                type: "combination"
+            },
+            {
+                id: "cl4",
+                main_img: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289657-9384876690289403.jpg",
+                hover_img: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289657-1754876690336179.jpg",
+                title: "SkinMedica AHA/BHA Exfoliating Cleanser (6oz)",
+                bPrice: "50.00",
+                aPrice: "48.00",
+                desc: "This exfoliating cleanser gently scrubs away dead skin using ultra-fine Jojoba Beads with Alpha-Hydroxy and Beta-Hydroxy Acids to improve the appearance of skin tone and texture.<br/><b>Benefits:</b><br><ul><li>Thoroughly removes environmental pollutants, excess oil and makeup.</li><li>Hydrates and refreshes skin.</li><li>Can be used by all skin types</li></ul>",
+                ingredients: "Water/Aqua/Eau, Sodium C14-16 Olefin Sulfonate, Cocamidopropyl Betaine, PEG/PPG-8/3 Diisostearate, Hydrogenated Jojoba Oil, Acrylates Copolymer, Vaccinium Myrtillus Fruit/Leaf Extract, Saccharum Officinarum Extract, Acer Saccharum Extract, Citrus Aurantium Dulcis Fruit Extract, Citrus Limon Fruit Extract, Salicylic Acid, Glycyrrhiza Glabra Root Extract, Panthenol, Bisabolol, Citric Acid, Glycerin, Ethylhexylglycerin, Sodium Hydroxide, Disodium EDTA, Potassium Sorbate, Phenoxyethanol, Lavandula Angustifolia Oil, Citrus Aurantium Dulcis Peel Oil, Mica, Tin Oxide, Titanium Dioxide (CI 77891).",
+                procedure: ["Moisten skin with warm water.", "Apply a small amount to the fingertips, add a little water and gently exfoliate and cleanse the face using small circular motions.", "Rinse thoroughly with warm water.", "Use as often as needed. Avoid getting in eyes. If contact occurs, rinse eyes thoroughly with water."],
+                reviews: 5,
+                gender: "women",
+                type: "normal"
             },
             
-    ]}
+    ]},
+    moisturisers: {
+        detail: {
+            heading: "Moisturizers",
+            qt: "1054 Results",
+            page_desc: "SkinStore offers a complete range of moisturizers which cater for every individual skin type and concern. Moisturizing your skin morning and night is one of the most important steps within your skin care regime, and a product perfectly designed for you can make all the difference. Featuring professional skin care products from brands such as NUXE, Elizabeth Arden and Erno Laszlo, you'll be sure to find the right one to look after your skin. From moisturizers aimed to nourish and hydrate, to night creams designed to prevent the signs of aging and vitality, your skin will be on it's way to looking its healthiest."
+        },
+        arr : [
+            {
+                id: "mo1",
+                main_img: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289613-6975043613167717.jpg",
+                hover_img: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289613-3764892771164923.jpg",
+                title: "SkinCeuticals Daily Moisture",
+                bPrice: "80.00",
+                aPrice: "76.00",
+                desc: "Protect and hydrate skin with <b>SkinCeuticals Daily Moisture</b>, a lightweight moisturizer that provides long-lasting hydration while minimizing the appearance of pores.<br/>Melting effortlessly into skin with no greasy residue, it intensely hydrates and nourishes, delivering a combination of Vitamin E and nutrient-rich Algae extracts to soften and replenish, while seven botanical extracts help to reduce the look of enlarged pores. Restoring and maintaining moisture, it will intensely hydrate while protecting against environmental damage.<br/> <i>Suitable for normal to oily skin.</i><br/><b>**Winner! Best Skincare Brand and Best Longstanding Brand 2017 as voted for by SkinStore customers.**</b>",
+                ingredients: "Aqua / Water / Eau, Glycerin, Cetyl Palmitate, Stearic Acid, Butylene Glycol, Palmitic Acid, Tocopherol, Dimethicone, Polymethylsilsesquioxane, Glyceryl Stearate, Sorbitol, Ci 77163 / Bismuth Oxychloride, Phenoxyethanol, Peg-100 Stearate, Sodium Hyaluronate, Hamamelis Virginiana Water / Witch Hazel Water, Peg-75 Stearate, Cetyl Alcohol, Carbomer, Ethylhexyl Hydroxystearate, Allantoin, Panthenol, Ceteth-20, Triethanolamine, Caprylyl Glycol, Xanthan Gum, Propylene Glycol, Steareth-20, Disodium Edta, Myristic Acid, Betaine, Chlorella Vulgaris Extract, Laminaria Saccharina Extract, Glucose, Chamomilla Recutita Flower Extract / Matricaria Flower Extract, Zingiber Officinale Root Extract / Ginger Root Extract, Sanguisorba Officinalis Root Extract, Cinnamomum Cassia Bark Extract, Thymus Vulgaris Flower/Leaf Extract - Thyme Flower/Leaf Extract, Retinyl Palmitate, Helianthus Annuus Seed Oil / Sunflower Seed Oil, Sodium Benzoate, Zinc Sulfate, Laminaria Ochroleuca Extract, Glycyrrhiza Glabra Extract / Licorice Root Extract, Urtica Dioica Extract / Nettle Extract, Equisetum Arvense Extract, Betula Alba Leaf Extract, Citric Acid, Potassium Sorbate, Cinnamal, Pyridoxine Hcl.",
+                procedure: ["Once or twice daily, apply a dime size of the moisturizer to the face, neck, and chest."],
+                reviews: 3,
+                gender: "women",
+                type: "oily"
+            },
+        ]
+    }
 }
 var giftsArray = [{
     id: "gift",
